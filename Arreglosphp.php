0264ebb4ab1002e530824b599800299f380da607
Wilmer Fernando Lainez Oyuela <sirmimex@hotmail.com>
2015-01-29 19:58:01 -0600

    Arreglos php

---
+++ Arreglosphp.php
@@ -0,0 +1,41 @@
+<?php
+//ejemplo de arreglos en PHP
+
+$arreglo=array(1,2,3,4,"texto",false,true);
+
+//motivos de depuracion
+print_r($arreglo);
+echo "<br/>";
+
+$arreglo2=array("Nombre"=>"Wilmer","Apellido"=>"Lainez","Edad"=>"00");
+print_r($arreglo2);
+echo "<br/>";
+
+$arreglo2[]="Esto no tiene llave";
+$arreglo2["titulo"]="Sir";
+print_r($arreglo2);
+echo "<br/>";
+
+$personas=array();
+$personas[]=$arreglo2;
+$personas[]=$arreglo;
+print_r($personas);
+echo "<br/>";
+/*
+$arreglo2["locura"]=$personas;
+$arreglo2[0]=$personas;
+print_r($personas);
+echo"<br/>";
+*/
+foreach($arreglo2 as $key =>$value){
+    echo "$key ->";
+    if(is_array($value)){
+        foreach($value as $key2=>$value2){
+            echo "<br/>.................$key2 -> $value2";
+        }
+    }else{
+        echo $value;
+    }
+    echo "<br/>";
+}
+?>
---
+++ Ejercicio2.php
@@ -0,0 +1,75 @@
+<?php
+$numero1=0;
+$numero2=0;
+$iteraciones=10;
+$msg="";
+
+if(isset($_POST["btnPrc"])){
+    //numero1 elevado al numero 2
+    $numero1=intval($_POST["numero1"]);
+    $numero2=intval($_POST["numero2"]);
+    $newValue=$numero1;
+    for($i=1;$i<$numero2;$i++){
+        $newValue*=$numero1;
+    }
+    $msg ="El valor: $numero1 a la potencia $numero2 es igual a $newValue";
+}
+if(isset($_POST["btnRev"])){
+    //echo numero1 * iteracion(a la inversa)
+    $numero1=intval($_POST["numero1"]);
+   $iteraciones=intval($_POST["iteraciones"]);
+   $contador=1;
+   while($iteraciones>0){
+    $msg .="$contador ). Producto:".($numero1*$iteraciones)."<br/>";
+$contador++;
+$iteraciones--;
+   }
+}
+if(isset($_POST["btnFac"])){
+    //factorial numero2
+    $numero2=intval($_POST["numero2"]);
+    $newVal=$numero2;
+    for($i=$numero2-1;$i>=1;$i--){
+        $newVal*=$i;
+    }
+    $msg ="El valor factorial de $numero2 es: $newVal";
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Ejercicio 2 Swicth y Ciclos</title>
+    </head>
+    <body>
+        <h1>Ejemplo de Switch y Ciclos en PHP</h1>
+        <form action="Ejercicio2.php" method="post">
+            <label for="numero1">Número 1</label>
+            <input type="text" id="numero1"
+                   name="numero1" value="<?php echo $numero1; ?>"/>
+            <br/>
+            <label for="numero2">Número 2</label>
+            <input type="text" id="numero2"
+                   name="numero2" value="<?php echo $numero2; ?>"/>
+            <br/>
+            <label for="interaciones">Número de Iteraciones</label>
+            <select name="iteraciones" id="iteraciones">
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="50">50</option>
+            </select>
+            <br/>
+            <input type="submit" value="Procesar"
+                   name="btnPrc" id="btnPrc"/>
+            <input type="submit" value="Reservar"
+                   name="btnRev" id="btnRev"/>
+            <input type="submit" value="Factorial"
+                   name="btnFac" id="btnFac"/>
+        </form>
+        <div>
+        <?php echo $msg;?>
+        </div>
+    </body>
+</html>
+    
